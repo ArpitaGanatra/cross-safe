8fe5d28308fa42b6aa3cfc3d3e6b74a6aa334dc5
ArpitaGanatra <ganatraarpita@gmail.com>
2023-10-19 23:48:06 +0530

    home page ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,30 +7,53 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className} homeBG`}
-    >
-      <div className="z-10 max-w-5xl ">
-        <h1 className="text-9xl font-bold mt-36">
-          Hassle free <span className="text-[#69f0ae]">multi-chain safe</span>
-        </h1>
-        <p className="text-2xl">
-          Fancy fact: Get the same address on all chains
-        </p>
+    // <main
+    //   className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className} homeBG`}
+    // >
+    //   <div className="z-10 max-w-5xl ">
+    //     <h1 className="text-9xl font-bold mt-36">
+    //       Hassle free <span className="text-[#69f0ae]">multi-chain safe</span>
+    //     </h1>
+    //     <p className="text-2xl">
+    //       Fancy fact: Get the same address on all chains
+    //     </p>
 
-        <div>
-          <Button colorScheme="green">
-            <Link href="/create-new">Create Safe</Link>
-          </Button>
-          <Button colorScheme="green">
-            <Link href="/fundsTransfer">Add/withdraw funds</Link>
-          </Button>
-          OR
-          <Button colorScheme="green">
-            <Link href="/existingInfo">Existing safe info</Link>
-          </Button>
+    //     <div>
+    //       <Button colorScheme="green">
+    //         <Link href="/create-new">Create Safe</Link>
+    //       </Button>
+    //       <Button colorScheme="green">
+    //         <Link href="/fundsTransfer">Add/withdraw funds</Link>
+    //       </Button>
+    //       OR
+    //       <Button colorScheme="green">
+    //         <Link href="/existingInfo">Existing safe info</Link>
+    //       </Button>
+    //     </div>
+    //   </div>
+    // </main>
+    <header className="bg-[#1E1e1e] h-screen bg-[url('/new-bg.svg')] bg-center">
+      <section className="w-[85%] mx-auto h-[95%] flex flex-col justify-between">
+        <div className="w-[80%] mx-auto py-32 text-center mt-16">
+          <h2 className="text-white  font-Grotesk text-5xl mb-6 tracking-wide leading-[50px]">
+            <span className="bg-gradient-to-r text-transparent bg-clip-text from-[#FD42FB] via-[#CD9ECD] to-[#753FF3] ">
+              Multi-chain Safes
+              <br />
+            </span>{" "}
+            One address - all chains
+          </h2>
+
+          <p className="text-gray-400 w-[60%] mx-auto font-Grotesk">
+            Deploy, transact and sign your safe to any chain. <br />
+            (Currently, we only have 2 chains due to time constraints)
+          </p>
+          <Link href={"/about"}>
+            <button className="py-3 px-8 mt-8 border font-Grotesk font-semibold border-gray-200 rounded-full bg-gradient-to-r text-transparent bg-clip-text from-[#FD42FB] via-[#CD9ECD] to-[#753FF3] hover:scale-105 transition-all 0.1s ease-in-out ">
+              Get Started
+            </button>
+          </Link>
         </div>
-      </div>
-    </main>
+      </section>
+    </header>
   );
 }
