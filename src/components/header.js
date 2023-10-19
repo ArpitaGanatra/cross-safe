4650f66eeddb574f374906a2a599c9acd5bd34a3
ArpitaGanatra <ganatraarpita@gmail.com>
2023-10-20 00:36:43 +0530

    navbar ui

--- src/components/header.js
+++ src/components/header.js
@@ -1,36 +1,35 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Link from "next/link";
 import React from "react";
-// import WalletSelector from "./walletSelector";
 
 const Header = () => {
   return (
-    <div
-      id={"head"}
-      className={
-        "fixed inset-x-0 top-0 z-50 w-full border-b border-primary-100"
-      }
-    >
-      <div className={"bg-primary-50/95"}>
-        <div className={"mx-auto flex flex-row justify-between p-4"}>
-          <div className={"flex items-center justify-start"}>
-            <Link href={"/"}>
-              <div
-                className={
-                  "flex items-center justify-center rounded-full bg-white p-2"
-                }
-              >
-                {/* <Logo className={"h-4 w-4"} /> */} safe-x
-              </div>
-            </Link>
-          </div>
+    <nav className=" text-white bg-[#1e1e1e] py-4 top-0 z-[20] fixed  w-full">
+      <div className="flex justify-between w-[90%] mx-auto items-center">
+        <Link href={"/"}>
+          <h2 className="font-Grotesk font-semibold text-2xl cursor-pointer">
+            SafeX
+          </h2>
+        </Link>
 
-          <div className={"flex items-center justify-end"}>
-            <ConnectButton />
-          </div>
+        <div className="flex gap-10 items-center">
+          <ul className="flex gap-14 uppercase font-thin text-sm tracking-widest">
+            <li className="hover:scale-105 hover:font-semibold cursor-pointer transition-all 0.1s ease-in-out">
+              <Link href="/create-new">Create Safe</Link>
+            </li>
+            <li className="hover:scale-105 hover:font-semibold cursor-pointer transition-all 0.1s ease-in-out">
+              <Link href="/fundsTransfer">Funds Transfer</Link>
+            </li>
+            <li className="hover:scale-105 hover:font-semibold cursor-pointer transition-all 0.1s ease-in-out">
+              <Link href="/existingInfo">Existing safe info</Link>
+            </li>
+          </ul>
+        </div>
+        <div className={"flex items-center justify-end"}>
+          <ConnectButton className="bg-purple-50" />
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
